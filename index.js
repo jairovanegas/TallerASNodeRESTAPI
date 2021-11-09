ab88b875439fce567ca18d4a7031620c9856931b
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2021-11-09 12:00:23 -0500

    Servidor express desplegado

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(cors({
+    origin: ["http://localhost", "http://localhost:8080"],
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.get("/", (req, res) => {
+    res.send({
+        data: "Esta consumiendo el servicio",
+        timestamp: Date.now()
+    });
+});
+const port = 4557;
+app.listen(port, () => {
+    console.log(`Escuchando en puerto ${port}`);
+});
